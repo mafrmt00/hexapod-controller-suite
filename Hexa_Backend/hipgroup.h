4307390496d342434760abb529cd7d837d639c03
frommleth@gmail.com <frommleth@gmail.com@4d999321-4534-94ab-e2b6-939512c78d94>
2012-12-07 14:46:15 +0000

    Merge, latest from target

--- Hexa_Backend/hipgroup.h
+++ Hexa_Backend/hipgroup.h
@@ -9,9 +9,14 @@ public:
 	int AddHip(CHip* pHip);
 
 	int SetPosition(double dPosition_X, double dPosition_Y, double dPosition_Z);
+
 	int SetPosition_X(double dPosition_X);
 	int SetPosition_Y(double dPosition_Y);
 	int SetPosition_Z(double dPosition_Z);
+
+	int SetPositionRel_X(double dPositionOffset_X);
+	int SetPositionRel_Y(double dPositionOffset_Y);
+	int SetPositionRel_Z(double dPositionOffset_Z);
 
 	int GetSSC32String(string& sConf);
 	int FinishSSC32String(string& sConf, int iMoveTime = 0);
